d5b80c7bd42f63cdc6c88e509c0865fae054a346
Abhijith <abhijithms1213@gmail.com>
2024-11-11 21:06:13 +0530

    3rd commit

--- reviewrush/lib/main.dart
+++ reviewrush/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reviewrush/features/home/presentation/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ScreenHome(),
     );
   }
 }
