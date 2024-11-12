ac1403750c33ec6db48ffcf778344d1fccb78651
Abhijith <abhijithms1213@gmail.com>
2024-11-12 23:14:22 +0530

    refractored code

--- reviewrush/lib/features/settings/presentation/widgets/action_button.dart
+++ reviewrush/lib/features/settings/presentation/widgets/action_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class ActionButton extends StatelessWidget {
   final IconData icon;
@@ -12,19 +13,30 @@ class ActionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: onTap,
-      icon: Container(
-        padding: const EdgeInsets.all(8),
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.2),
-          shape: BoxShape.circle,
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                // color: Colors.white.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child:  Center(
+                child: Icon(
+                  icon,
+                  color: const Color.fromARGB(175, 255, 255, 255),
+                  size: 28,
+                ),
+              ),
+            ),
+          ),
         ),
-        child: Icon(
-          icon,
-          color: Colors.white,
-        ),
-      ),
+      ],
     );
   }
 }
