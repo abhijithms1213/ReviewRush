928220d737af5c138fa87c59025904948029d99b
Abhijith <abhijithms1213@gmail.com>
2024-11-13 11:27:19 +0530

    message _widget added

--- reviewrush/lib/features/chat_ui/presentation/widgets/chat_ui_widget.dart
+++ reviewrush/lib/features/chat_ui/presentation/widgets/chat_ui_widget.dart
@@ -1,1 +1,53 @@
-// Widget file 
+import 'package:flutter/material.dart';
+import 'package:reviewrush/features/chat_ui/data/models/chat_message.dart';
+
+class MessageBubble extends StatelessWidget {
+  final ChatMessage message;
+
+  const MessageBubble({
+    super.key,
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment:
+            message.isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+        children: [
+          if (!message.isUser)
+            const CircleAvatar(
+              radius: 15,
+              backgroundColor: Colors.red,
+              child: Icon(Icons.audiotrack_rounded, color: Colors.white),
+            ),
+          // const SizedBox(width: 8),
+          Flexible(
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.75,
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 10.0,
+              ),
+              decoration: BoxDecoration(
+                color: message.isUser ? Colors.grey[300] : Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Text(
+                message.text,
+                style: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
