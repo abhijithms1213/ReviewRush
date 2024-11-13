8e871b7d28ea350768257a5f851659815c18e473
Abhijith <abhijithms1213@gmail.com>
2024-11-13 11:52:07 +0530

    modified ui

--- reviewrush/lib/features/chat_ui/presentation/widgets/custom_app_bar.dart
+++ reviewrush/lib/features/chat_ui/presentation/widgets/custom_app_bar.dart
@@ -1,72 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:reviewrush/core/constants/spacing.dart';
 
 class ChatTopBar extends StatelessWidget implements PreferredSizeWidget {
   const ChatTopBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(20.0),
-          bottomRight: Radius.circular(20.0),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 10,
-            offset: const Offset(0, 5),
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          const SizedBox(width: 12.0),
-          const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Marie Wondy',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16.0,
-                ),
-              ),
-              Text(
-                'Active now',
-                style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 12.0,
-                ),
-              ),
-            ],
-          ),
-          const Spacer(),
-          Container(
+    return Column(
+      children: [
+        khbox10,
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Container(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
             decoration: BoxDecoration(
-              color: Colors.purple.withOpacity(0.1),
-              borderRadius: BorderRadius.circular(15.0),
+              color: Colors.white,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(20.0),
+                bottomRight: Radius.circular(20.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: const Offset(0, 5),
+                ),
+              ],
             ),
-            padding: const EdgeInsets.all(8.0),
-            child: const Icon(
-              Icons.call,
-              color: Colors.purple,
+            child: Row(
+              children: [
+                const SizedBox(width: 12.0),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Module -1',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    Text(
+                      'write something...',
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.purple.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Icon(
+                    Icons.arrow_back_ios_new,
+                    color: Colors.purple,
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
   @override
   Size get preferredSize =>
-      const Size.fromHeight(60.0); // Set the height of the custom app bar
+      const Size.fromHeight(70.0); // Set the height of the custom app bar
 }
