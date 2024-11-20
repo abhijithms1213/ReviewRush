c1c36461d7a592b99b80443afca981f1f574c6fb
Abhijith <abhijithms1213@gmail.com>
2024-11-20 12:53:20 +0530

    normal chat sending, no consistancy

--- reviewrush/lib/features/chat_ui/presentation/bloc/gpt_bloc.dart
+++ reviewrush/lib/features/chat_ui/presentation/bloc/gpt_bloc.dart
@@ -4,42 +4,45 @@ import 'package:reviewrush/features/chat_ui/domain/usecases/chat_ui_use_case.dar
 
 part 'gpt_event.dart';
 part 'gpt_state.dart';
+
 class GPTBloc extends Bloc<GPTEvent, GPTState> {
   final GetChatResponseUseCase getChatResponse;
 
   GPTBloc(this.getChatResponse) : super(GPTInitial()) {
-    on<SendMessageEvent>((event, emit) async {
-      final currentState = state;
-      if (currentState is GPTLoaded) {
-        emit(GPTLoading());
-        try {
-          final response = await getChatResponse(event.message);
-          final newMessage = ChatMessageModel(
-            text: response,
-            isUser: false,
-          );
-          final updatedMessages = List<ChatMessageModel>.from(
-              currentState.messages)
-            ..add(ChatMessageModel(text: event.message, isUser: true))
-            ..add(newMessage);
-          emit(GPTLoaded(updatedMessages));
-        } catch (e) {
-          emit(GPTError('Failed to fetch response: $e'));
+    on<SendMessageEvent>(
+      (event, emit) async {
+        final currentState = state;
+        if (currentState is GPTLoaded) {
+          emit(GPTLoading());
+          try {
+            final response = await getChatResponse(event.message);
+            final newMessage = ChatMessageModel(
+              text: response,
+              isUser: false,
+            );
+            final updatedMessages =
+                List<ChatMessageModel>.from(currentState.messages)
+                  ..add(ChatMessageModel(text: event.message, isUser: true))
+                  ..add(newMessage);
+            emit(GPTLoaded(updatedMessages));
+          } catch (e) {
+            emit(GPTError('Failed to fetch response: $e'));
+          }
+        } else {
+          emit(GPTLoading());
+          try {
+            final response = await getChatResponse(event.message);
+            emit(
+              GPTLoaded([
+                ChatMessageModel(text: event.message, isUser: true),
+                ChatMessageModel(text: response, isUser: false),
+              ]),
+            );
+          } catch (e) {
+            emit(GPTError('Failed to fetch response: $e'));
+          }
         }
-      } else {
-        emit(GPTLoading());
-        try {
-          final response = await getChatResponse(event.message);
-          emit(
-            GPTLoaded([
-              ChatMessageModel(text: event.message, isUser: true),
-              ChatMessageModel(text: response, isUser: false),
-            ]),
-          );
-        } catch (e) {
-          emit(GPTError('Failed to fetch response: $e'));
-        }
-      }
-    });
+      },
+    );
   }
 }
